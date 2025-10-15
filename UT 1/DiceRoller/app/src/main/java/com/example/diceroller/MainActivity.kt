356d1b78e6ee566232c5d6b6b256d4b8541a8a9f
ALVAROPIOS <40148175+AeroAlvaro@users.noreply.github.com>
2025-10-15 18:40:39 +0200

    Dos botones

--- UT 1/DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ UT 1/DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -20,7 +20,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.diceroller.ui.theme.DiceRollerTheme
@@ -48,18 +47,9 @@ fun DiceRollerApp() {
 @Composable
 fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
     var result1 by remember { mutableStateOf(1) }
-    var result2 by remember { mutableStateOf(1) }
+    var result2 by remember { mutableStateOf(0) } // 0 significa que no se ha lanzado
 
     val imageResource1 = when (result1) {
-        1 -> R.drawable.dice_1
-        2 -> R.drawable.dice_2
-        3 -> R.drawable.dice_3
-        4 -> R.drawable.dice_4
-        5 -> R.drawable.dice_5
-        else -> R.drawable.dice_6
-    }
-
-    val imageResource2 = when (result2) {
         1 -> R.drawable.dice_1
         2 -> R.drawable.dice_2
         3 -> R.drawable.dice_3
@@ -74,21 +64,41 @@ fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
     ) {
         Row {
             Image(
-                painter = painterResource(imageResource1),
+                painter = painterResource(id = imageResource1),
                 contentDescription = result1.toString()
             )
-            Image(
-                painter = painterResource(imageResource2),
-                contentDescription = result2.toString()
-            )
+            if (result2 != 0) {
+                val imageResource2 = when (result2) {
+                    1 -> R.drawable.dice_1
+                    2 -> R.drawable.dice_2
+                    3 -> R.drawable.dice_3
+                    4 -> R.drawable.dice_4
+                    5 -> R.drawable.dice_5
+                    else -> R.drawable.dice_6
+                }
+                Image(
+                    painter = painterResource(id = imageResource2),
+                    contentDescription = result2.toString()
+                )
+            }
         }
 
         Spacer(modifier = Modifier.height(16.dp))
+
+        Button(onClick = {
+            result1 = (1..6).random()
+            result2 = 0
+        }) {
+            Text("Lanzar un dado")
+        }
+
+        Spacer(modifier = Modifier.height(8.dp))
+
         Button(onClick = {
             result1 = (1..6).random()
             result2 = (1..6).random()
         }) {
-            Text(stringResource(R.string.roll))
+            Text("Lanzar los dos dados")
         }
     }
 }
