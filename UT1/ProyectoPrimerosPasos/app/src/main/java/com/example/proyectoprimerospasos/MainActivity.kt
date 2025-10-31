9ee9271b68378e6bcf3c5d1ec64a579fab1f5d2b
ALVAROPIOS <40148175+AeroAlvaro@users.noreply.github.com>
2025-10-31 16:23:15 +0100

    Elegir alumno aleatorio
    No recuerda los que ya han salido

--- UT1/ProyectoPrimerosPasos/app/src/main/java/com/example/proyectoprimerospasos/MainActivity.kt
+++ UT1/ProyectoPrimerosPasos/app/src/main/java/com/example/proyectoprimerospasos/MainActivity.kt
@@ -1,20 +1,50 @@
 package com.example.proyectoprimerospasos
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var tvAlumnoSeleccionado: TextView
+    private lateinit var tvListaAlumnos: TextView
+    private lateinit var btnSiguiente: Button
+
+    private var alumnosLista = mutableListOf<String>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        tvAlumnoSeleccionado = findViewById(R.id.tvAlumnoSeleccionado)
+        tvListaAlumnos = findViewById(R.id.tvListaAlumnos)
+        btnSiguiente = findViewById(R.id.btnSiguiente)
+
+        alumnosLista.addAll(listOf("Álvaro", "Carlos", "Santiago", "Helena"))
+
+        actualizarListaVisual()
+
+        btnSiguiente.setOnClickListener {
+            seleccionarSiguiente()
         }
+    }
+
+    private fun seleccionarSiguiente() {
+        val indexAleatorio = Random.nextInt(alumnosLista.size)
+        val alumnoElegido = alumnosLista[indexAleatorio]
+        tvAlumnoSeleccionado.text = alumnoElegido
+    }
+
+    private fun actualizarListaVisual() {
+        val textoBuilder = StringBuilder()
+        textoBuilder.append("Alumnos:\n\n")
+
+        alumnosLista.forEach { alumno ->
+            textoBuilder.append("$alumno\n")
+        }
+
+        tvListaAlumnos.text = textoBuilder.toString()
     }
 }
